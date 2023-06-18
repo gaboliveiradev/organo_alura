794f649e1d2cb9fed4351e96cb0985bdf2408118
gaboliveiradev <g38789138@gmail.com>
2023-06-17 21:42:40 -0300

    feat: criando o componente de select

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,11 @@
 import './Form.css';
 import InputText from '../InputText';
+import Select from "../Select";
 
 const Form = () => {
+
+    const times = ['Programação', 'Front-End', 'Data Science', 'Devops', 'UX Desing', 'Mobile', 'Inovação e Gestão']
+
     return (
         <section className='form'>
             <form>
@@ -9,6 +13,7 @@ const Form = () => {
                 <InputText label="Nome" placeholder="Digite o seu nome" />
                 <InputText label="Cargo" placeholder="Digite o seu cargo"/>
                 <InputText label="Imagem" placeholder="Informe o endereço da imagem"/>
+                <Select label="Time" itens={times} />
             </form>
         </section>
     )
